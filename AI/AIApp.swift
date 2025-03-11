31128fcfd200b23f86499e8ecdb5ccf924c7de70
Jchillah <jchillah@gmail.com>
2025-03-11 14:32:18 +0100

    Initial Commit

---
+++ AI/AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIApp.swift
+//  AI
+//
+//  Created by Michael Winkler on 11.03.25.
+//
+
+import SwiftUI
+
+@main
+struct AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
